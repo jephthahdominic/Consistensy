474d20b71f65e91544b725959e196f86aa65c7a4
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-01-03 04:05:59 +0100

    Refactored the form logic appropriately

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,19 @@
 import { useAppContext } from '../contexts/AppContext'
+import { useTheme } from '../contexts/ThemeContext';
 import styles from './Loader.module.css'
 
 export default function Loader(){
-    const {dispatch} = useAppContext()
+    const {dispatch} = useAppContext();
+    const themeContext = useTheme()
+    const {theme} = themeContext
     setTimeout(()=>{
         dispatch({type:"loaded"})
-    }, 300)
+    }, 500)
     return(
-        <div className={styles.loader}>
-            <div className={styles.spinner}>
+        <div className={`${styles.loader} ${theme==='dark'? styles.darkLoader : styles.lightLoader}`}>
+            <div className={`${styles.spinner} ${theme==='dark'? styles.darkSpinner : styles.lightSpinner}`}>
                 <div className={styles.spinnerInner}>
-                    <div className={styles.spinnerCircle}></div>
+                    <div className={styles.darkSpinnerCircle}></div>
                 </div>
             </div>
         </div>
