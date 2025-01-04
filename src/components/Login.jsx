22c12686644940aaf0af180fb3ce902267868f64
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-01-04 02:15:11 +0100

    Adding a email registration form for notifications

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,15 +2,15 @@ import { MdArrowRight } from 'react-icons/md'
 import Button from './Button'
 import styles from './Login.module.css'
 import { FaArrowRight } from 'react-icons/fa'
+import { CgGoogle } from 'react-icons/cg'
 
 export default function Login() {
   return (
     <div className={styles.login}>
-      <h2>EMAIL REGISTRATION</h2>
-      <p>Kindly enter your email to continue to ruth<span>i</span>ne</p>
+      <h2>CREATE ACCOUNT</h2>
+      <p>Click the link below to signup with Google.</p>
       <form action="">
-          <input type="Email" autoFocus/>
-          <Button>Proceed <FaArrowRight /></Button>
+          <button><CgGoogle /> Login with Google</button>
       </form>
     </div>
   )
