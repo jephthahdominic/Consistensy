80bcabb2e5d6cb5fea5cadff95c5989bfa5bec5c
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-01-06 13:29:38 +0100

    built the landing page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,25 @@
-import { getAuth, signInWithRedirect } from 'firebase/auth'
-import { provider} from '../../FirebaseConfig'
+import { signInWithRedirect } from 'firebase/auth'
+import { auth, provider} from '../../FirebaseConfig'
 
 import styles from './Login.module.css'
 import { CgGoogle } from 'react-icons/cg'
+import { MdEmail } from 'react-icons/md'
 
 export default function Login() {
-  const auth = getAuth()
   function signInWithGoogle(e){
     e.preventDefault()
-    signInWithRedirect(auth, provider).then((result) => console.log(result))
+    signInWithRedirect(auth, provider);
   }
   return (
     <div className={styles.login}>
-      <h2>Sign up with google to start tracking!</h2>
-      <p>Click the link below to signup with Google.</p>
+      <h2>Sign up with your Email to start tracking!</h2>
+      <p>Enter your email address below to register.</p>
       <form>
-          <button onClick={(e)=>signInWithGoogle(e)}><CgGoogle /> Login with Google</button>
+        <div className={styles.email}>
+          <input type="email" id='email'/>
+          <span><MdEmail/></span>
+        </div>
+        <button>Verify</button>
       </form>
     </div>
   )
