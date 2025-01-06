a23bd91d9be109c083b5276a9f1cdc6704b247dc
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-01-06 09:15:05 +0100

    built the landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -10,11 +10,16 @@ export default function LandingPage() {
       </header>
       <main className={styles.landingPage_main}>
         <h1>Turn Habits into Happiness—Track Your Routine, Transform Your Life!</h1>
-        <p>To begin your journey of consistency, click the link below to sign up with your google account and get started.</p>
-        <button>
-          <img src={google} alt="google icon" width={30} height={30}/>
-          Sign up with Google
-        </button>
+        <p>Whether you're looking to boost productivity, improve your health, or simply find balance in your daily life, 
+          our intuitive tracker is here to guide and support you every step of the way.
+        </p>
+        <div>
+          <p>To begin your journey of consistency, click the link below to sign up with your google account and get started.</p>
+          <button>
+            <img src={google} alt="google icon" width={30} height={30}/>
+            Sign up with Google
+          </button>
+        </div>
       </main>
     </div>
   )
